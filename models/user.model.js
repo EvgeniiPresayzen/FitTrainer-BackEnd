a18409b2788b520b019f4b9a00e7e9753d336d3c
EvgeniiPresayzen <midnightzp95@gmail.com>
2018-11-08 10:23:39 +0200

    update userModel

--- models/user.model.js
+++ models/user.model.js
@@ -1,44 +1,36 @@
 const mongoose = require('mongoose'),
   Schema = mongoose.Schema,
   bcrypt = require('bcrypt'),
-  SALT_WORK_FACTOR = 10;
+  SALT_WORK_FACTOR = 10
 
 const UserSchema = new Schema({
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
-  active: { type: Boolean, required: true, default: false}
-});
+  active: { type: Boolean, required: true, default: false }
+}, {
+  timestamps: {
+    createAt: 'createAt',
+    updateAt: 'updateAt'
+  }
+}
+})
 
 
-UserSchema.pre('save', function(next) {
-  const user = this;
+const User = mongoose.model('user', UserSchema)
+module.exports = User
+module.exports.hashPassword = async (password) => {
+  try {
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR)
+    return await bcrypt.hash(password, salt)
+  } catch (error) {
+    throw new Error('Hashing failed', error)
+  }
+}
 
-  // only hash the password if it has been modified (or is new)
-  if (!user.isModified('password')) return next();
-
-  // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if (err) return next(err);
-
-    // hash the password using our new salt
-    bcrypt.hash(user.password, salt, function(err, hash) {
-      if (err) return next(err);
-
-      // override the cleartext password with the hashed one
-      user.password = hash;
-      next();
-    });
-  });
-});
-
-UserSchema.methods.comparePassword = (candidatePassword, cb) => {
-  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
-};
-
-//2
-
-// Export the model
-module.exports = mongoose.model('User', UserSchema)
+module.exports.comparePasswords = async (inputPassword, hashedPassword) => {
+  try {
+    return await bcrypt.compare(inputPassword, hashedPassword)
+  } catch (error) {
+    throw new Error('Comparing failed', error)
+  }
+}
